3e3d7ef5735cde74cd0a709cad9fb53fc53c04ff
Stefan Köhler <mail@skoehler.net>
2024-06-09 22:00:57 +0200

    Style nav card

--- widgets/cards/nav-card/index.php
+++ widgets/cards/nav-card/index.php
@@ -80,18 +80,20 @@ class Eperi_Nav_Card_Widget extends \Elementor\Widget_Base
         $settings = $this->get_settings_for_display();
 ?>
 
-        <div style="background-color:red; color:white; display:inline; padding:0px 5px">Nav Card</div>
+        <!-- <div style="background-color:red; color:white; display:inline; padding:0px 5px">Nav Card</div> -->
 
-        <div class="navCard">
+        <div class="navCard border_radius_16">
 
-            <h2><?php echo $settings['title']; ?></h2>
-            <?php if (!empty($settings['link']['url'])) {
-                $this->add_link_attributes('link', $settings['link']);
-            }
-            ?>
-            <a <?php echo $this->get_render_attribute_string('link'); ?>>
-                Add link here
-            </a>
+            <h3 class="title_size_3 "><?php echo $settings['title']; ?></h3>
+            <div class="navLink">
+                <?php if (!empty($settings['link']['url'])) {
+                    $this->add_link_attributes('link', $settings['link']);
+                }
+                ?>
+                <a <?php echo $this->get_render_attribute_string('link'); ?>><svg width="24" height="24" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M17.3452 6.67038L9.25865 0.0889053C8.99147 -0.124837 8.59961 0.0710926 8.59961 0.418424V6.00077C8.58647 6.00026 8.57327 6 8.56 6L1.5 6C0.947715 6 0.5 6.44772 0.5 7C0.5 7.55229 0.947715 8 1.5 8L8.56 8C8.57327 8 8.58647 7.99974 8.59961 7.99923V13.5903C8.59961 13.9287 8.99147 14.1246 9.24974 13.9109L17.3452 7.32051C17.5501 7.1513 17.5501 6.83959 17.3452 6.67038Z" fill="black" />
+                    </svg></a>
+            </div>
         </div>
 
 <?php

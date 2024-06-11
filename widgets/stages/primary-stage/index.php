44f39d1cea6aa31ee837714a4c4565591b21fc70
Stefan Köhler <mail@skoehler.net>
2024-06-11 15:31:29 +0200

    Fix primary stage

--- widgets/stages/primary-stage/index.php
+++ widgets/stages/primary-stage/index.php
@@ -44,6 +44,26 @@ class Eperi_Primary_Stage_Widget extends \Elementor\Widget_Base
         );
 
         $this->add_control(
+            'headline',
+            [
+                'label' => esc_html__('Headline', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::TEXTAREA,
+                // 'default' => esc_html__('Add your headline here', 'textdomain'),
+                'placeholder' => esc_html__('Type your headline here', 'textdomain'),
+            ]
+        );
+
+        $this->add_control(
+            'text',
+            [
+                'label' => esc_html__('Text', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::TEXTAREA,
+                // 'default' => esc_html__('Add your headline here', 'textdomain'),
+                'placeholder' => esc_html__('Type your text here', 'textdomain'),
+            ]
+        );
+
+        $this->add_control(
             'image',
             [
                 'label' => esc_html__('Choose Image', 'textdomain'),
@@ -51,6 +71,53 @@ class Eperi_Primary_Stage_Widget extends \Elementor\Widget_Base
                 'default' => [
                     'url' => \Elementor\Utils::get_placeholder_image_src(),
                 ],
+            ]
+        );
+
+        $this->add_control(
+            'topImage',
+            [
+                'label' => esc_html__('Choose Top Image', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src(),
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'label',
+            [
+                'label' => esc_html__('Button Label', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => esc_html__('Add label here', 'textdomain'),
+            ]
+        );
+
+
+        $this->add_control(
+            'cta',
+            [
+                'label' => esc_html__('Call to action', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::URL,
+                'options' => ['url', 'is_external', 'nofollow'],
+                'default' => [
+                    'url' => '',
+                    'is_external' => true,
+                    'nofollow' => true,
+                    // 'custom_attributes' => '',
+                ],
+                'label_block' => true,
+            ]
+        );
+
+        $this->add_control(
+            'hubSpotCTA',
+            [
+                'label' => esc_html__('HubSpot CTA', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::TEXTAREA,
+                // 'default' => esc_html__('Add your title here', 'textdomain'),
+                'placeholder' => esc_html__('Type your title here', 'textdomain'),
             ]
         );
 
@@ -65,19 +132,36 @@ class Eperi_Primary_Stage_Widget extends \Elementor\Widget_Base
         <div class="primaryStage bottom_margin_56 top_margin_40">
             <div class="">
                 <div class="subline">Pagecategory – current Page title</div>
-                <div class="title_size_1">Headline Primary Stage pagetitle</div>
+                <div class="title_size_1"><?php echo nl2br($settings['headline']); ?></div>
             </div>
 
-            <div class="top_margin_56" style="display: grid; grid-template-columns: 0.3fr 0.7fr; gap:100px">
+            <div class="top_margin_56" style="display: grid; grid-template-columns: 0.4fr 0.6fr; gap:100px">
                 <div style="display:block">
                     <div style="position:relative; border-left:1px solid #00B4B4; padding-left:115px;">
-                        <div class="copy_medium">Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. </div>
-                        <div class="button button_light_primary top_margin_56">Eperi Button</div>
+                        <div class="copy_medium" style="width:100%"><?php echo $settings['text']; ?></div>
+                        <?php
+
+                        if (!empty($settings['hubSpotCTA'])) {
+                            echo $settings['hubSpotCTA'];
+                        } else { ?>
+                            <div class="button button_light_primary top_margin_56">
+                                <?php if (!empty($settings['cta']['url'])) {
+                                    $this->add_link_attributes('cta', $settings['cta']);
+                                }
+                                ?>
+                                <a <?php echo $this->get_render_attribute_string('cta'); ?>>
+                                    <?php echo !empty($settings['label']) ? $settings['label'] : 'Mehr erfahren'; ?>
+                                </a>
+                            </div>
+                        <?php } ?>
                         <div style="position:absolute; top:0px; left:0px; background-color:#00B4B4; width:4px; height:24px; border-radius: 0 0 4px 0"></div>
                     </div>
                 </div>
                 <div><img src="<?php echo esc_url($settings['image']['url']); ?>" alt="" class="default_image border_radius_24"></div>
             </div>
+
+            <div class="topimage"><img src="<?php echo esc_url($settings['topImage']['url']); ?>" alt="" class="default_image border_radius_24"></div>
+
         </div>
 
 <?php

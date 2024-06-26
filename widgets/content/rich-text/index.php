b38e197e7f68c7c3edc4e34631f892b6b3dc81f6
Stefan Köhler <mail@skoehler.net>
2024-06-26 08:46:57 +0200

    Remove background

--- widgets/content/rich-text/index.php
+++ widgets/content/rich-text/index.php
@@ -123,8 +123,8 @@ class Eperi_Rich_Text_Widget extends \Elementor\Widget_Base
                 height: 24px;
                 /* Define the height of the icon */
 
-                background-color: #00b4b4 !important;
-                /* -webkit-mask-image: url('<?php echo esc_url($settings['bulletIcon']['url']) ?>'); */
+                /* background-color: #00b4b4 !important; */
+                -webkit-mask-image: url('<?php echo esc_url($settings['bulletIcon']['url']) ?>');
                 mask-image: url('<?php echo esc_url($settings['bulletIcon']['url']) ?>');
                 /* fill: #00b4b4;
                 color: #00b4b4; */
